c3e66ff45fe9b9e16c71e433dc34a432bf421b71
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-08-13 16:19:36 +0200

    function updates

--- steps/main.c
+++ steps/main.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <xmethula@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 17:08:59 by xmethula          #+#    #+#             */
-/*   Updated: 2019/08/13 13:06:03 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/08/13 16:11:59 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,26 @@ void	print_lst(t_stack *node)
 	}
 }
 
+/*int		lst_len(t_stack *lst)
+{
+	int		len;
+
+	len = 0;
+	while (lst->next != NULL)
+	{
+		len++;
+		lst = lst->next;
+	}
+	ft_putnbr_endl(lst->num);
+	while (lst->prev != NULL)
+	{
+		len--;
+		lst = lst->prev;
+	}
+	ft_putnbr_endl(lst->num);
+	return (len);
+}*/
+
 void	add_end(t_stack **node, int num)
 {
 	t_stack		*new;
@@ -35,6 +55,7 @@ void	add_end(t_stack **node, int num)
 	while (i->next)
 		i = i->next;
 	i->next = new;
+	new->prev = i;
 }
 
 int		main(void)
