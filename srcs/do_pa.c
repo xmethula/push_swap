833110f809f473710be5a0877e1ed651bb9952dc
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-08-15 17:38:18 +0200

    srcs/

---
+++ srcs/do_pa.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   do_pa.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xmethula <xmethula@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/15 17:19:26 by xmethula          #+#    #+#             */
+/*   Updated: 2019/08/15 17:25:16 by xmethula         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/libft.h"
+#include "../includes/push_swap.h"
+
+void    do_pa(t_stack *stack_a, t_stack *stack_b)
+{
+    long int    tmp;
+
+    if (stack_b != NULL)
+    {
+        tmp = stack_b->num;
+        stack_del_top(stack_b);
+        stack_add_top(stack_a, tmp);
+    }
+}
