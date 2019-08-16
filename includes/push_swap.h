6d94e87e1dea22c1035d03c262cd9be8c379a1e7
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-08-16 17:33:01 +0200

    includes/

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: xmethula <xmethula@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 15:59:58 by xmethula          #+#    #+#             */
-/*   Updated: 2019/08/16 14:30:30 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/08/16 17:27:50 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ void	stack_del_top(t_stack **stack);
 void	stack_add_top(t_stack **stack, long int num);
 
 size_t	stack_len(t_stack *stack);
+int		duplicates(t_stack *stack);
 
 #endif
