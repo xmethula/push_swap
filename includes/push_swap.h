f2ec68c67d60e2ab9d2659d37a20969dc7fe4e51
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-08-16 14:49:51 +0200

    includes/

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: xmethula <xmethula@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 15:59:58 by xmethula          #+#    #+#             */
-/*   Updated: 2019/08/15 17:17:46 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/08/16 14:30:30 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,11 @@ typedef struct			s_stack
 void	do_sa(t_stack *stack);
 void	do_ra(t_stack *stack);
 void	do_rra(t_stack *stack);
-void	do_pa(t_stack *stack_a, t_stack *stack_b);
+void	do_pa(t_stack **stack_a, t_stack **stack_b);
+void	do_pb(t_stack **stack_a, t_stack **stack_b);
 void	stack_del_top(t_stack **stack);
 void	stack_add_top(t_stack **stack, long int num);
+
+size_t	stack_len(t_stack *stack);
 
 #endif
