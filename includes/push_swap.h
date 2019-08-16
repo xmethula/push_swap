1f8fd4efd7c90ba91839d6f997c2f94662d12f21
x.r.methula@gmail.com <ray>
2019-08-16 20:54:40 +0200

    includes/

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -20,15 +20,16 @@ typedef struct			s_stack
 	struct s_stack		*next;
 }						t_stack;
 
-void	do_sa(t_stack *stack);
-void	do_ra(t_stack *stack);
-void	do_rra(t_stack *stack);
-void	do_pa(t_stack **stack_a, t_stack **stack_b);
-void	do_pb(t_stack **stack_a, t_stack **stack_b);
-void	stack_del_top(t_stack **stack);
-void	stack_add_top(t_stack **stack, long int num);
+void		do_sa(t_stack *stack);
+void		do_ra(t_stack *stack);
+void		do_rra(t_stack *stack);
+void		do_pa(t_stack **stack_a, t_stack **stack_b);
+void		do_pb(t_stack **stack_a, t_stack **stack_b);
+void		stack_del_top(t_stack **stack);
+void		stack_add_top(t_stack **stack, long int num);
 
-size_t	stack_size(t_stack *stack);
-int		duplicates(t_stack *stack);
+size_t		stack_size(t_stack *stack);
+int			duplicates(t_stack *stack);
+int			is_sorted(t_stack *stack);
 
 #endif
