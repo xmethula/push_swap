da052903ca2bf4c39ad0b57ae85993110df7441e
x.r.methula@gmail.com <ray>
2019-08-17 21:08:32 +0200

    function update

--- srcs/display_stack.c
+++ srcs/display_stack.c
@@ -6,12 +6,12 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/17 17:22:13 by xmethula          #+#    #+#             */
-/*   Updated: 2019/08/17 17:24:41 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/08/17 20:53:13 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
-#include "includes/push_swap.h"
+#include "../includes/push_swap.h"
 
 void	display_stack(t_stack *stack)
 {
