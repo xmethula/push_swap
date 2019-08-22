0825d949c2d94ad78acf07698cf52e450db9e7c7
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-08-22 08:34:31 +0200

    includes/

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: xmethula <xmethula@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 15:59:58 by xmethula          #+#    #+#             */
-/*   Updated: 2019/08/21 16:19:44 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/08/22 08:24:02 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,17 @@ typedef struct			s_stack
 	struct s_stack		*next;
 }						t_stack;
 
-void		do_sa(t_stack *stack);
-void		do_ss(t_stack *stack_a, t_stack *stack_b);
-void		do_ra(t_stack *stack);
-void		do_rr(t_stack *stack_a, t_stack *stack_b);
-void		do_rra(t_stack *stack);
-void		do_rrr(t_stack *stack_a, t_stack *stack_b);
-void		do_pa(t_stack **stack_a, t_stack **stack_b);
-void		do_pb(t_stack **stack_a, t_stack **stack_b);
+void		do_sa(t_stack *stack, int flag);
+void		do_sb(t_stack *stack, int flag);
+void		do_ss(t_stack *stack_a, t_stack *stack_b, int flag);
+void		do_ra(t_stack *stack, int flag);
+void		do_rb(t_stack *stack, int flag);
+void		do_rr(t_stack *stack_a, t_stack *stack_b, int flag);
+void		do_rra(t_stack *stack, int flag);
+void		do_rrb(t_stack *stack, int flag);
+void		do_rrr(t_stack *stack_a, t_stack *stack_b, int flag);
+void		do_pa(t_stack **stack_a, t_stack **stack_b, int flag);
+void		do_pb(t_stack **stack_a, t_stack **stack_b, int flag);
 void		stack_del_top(t_stack **stack);
 void		stack_add_top(t_stack **stack, long int num);
 void		fill_stack(int argc, char **argv, t_stack **stack);
@@ -37,6 +40,7 @@ void		display_stack(t_stack *stack);
 size_t		stack_len(t_stack *stack);
 int			duplicates(t_stack *stack);
 int			is_sorted(t_stack *stack);
+
 
 
 void		sort_5_or_less(t_stack **stack_a, t_stack **stack_b);
