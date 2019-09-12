fc4c1d35ebdb5e38a44edb796f1105d49f4f4083
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-09-12 11:23:02 +0200

    includes/

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: xmethula <xmethula@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 10:44:19 by xmethula          #+#    #+#             */
-/*   Updated: 2019/09/11 16:30:37 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/09/12 10:18:54 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,28 @@ typedef struct			s_stack
 	struct s_stack		*next;
 }						t_stack;
 
-void		do_sa(t_stack *stack, int flag);
-void		do_sb(t_stack *stack, int flag);
-void		do_ss(t_stack *stack_a, t_stack *stack_b, int flag);
-void		do_ra(t_stack *stack, int flag);
-void		do_rb(t_stack *stack, int flag);
-void		do_rr(t_stack *stack_a, t_stack *stack_b, int flag);
-void		do_rra(t_stack *stack, int flag);
-void		do_rrb(t_stack *stack, int flag);
-void		do_rrr(t_stack *stack_a, t_stack *stack_b, int flag);
-void		do_pa(t_stack **stack_a, t_stack **stack_b, int flag);
-void		do_pb(t_stack **stack_a, t_stack **stack_b, int flag);
-void		fill_stack(int argc, char **argv, t_stack **stack);
-void		read_stdin(t_stack **stack_a, t_stack **stack_b);
+void					do_sa(t_stack *stack, int flag);
+void					do_sb(t_stack *stack, int flag);
+void					do_ss(t_stack *stack_a, t_stack *stack_b, int flag);
+void					do_ra(t_stack *stack, int flag);
+void					do_rb(t_stack *stack, int flag);
+void					do_rr(t_stack *stack_a, t_stack *stack_b, int flag);
+void					do_rra(t_stack *stack, int flag);
+void					do_rrb(t_stack *stack, int flag);
+void					do_rrr(t_stack *stack_a, t_stack *stack_b, int flag);
+void					do_pa(t_stack **stack_a, t_stack **stack_b, int flag);
+void					do_pb(t_stack **stack_a, t_stack **stack_b, int flag);
+void					fill_stack(int argc, char **argv, t_stack **stack);
+int						stack_len(t_stack *stack);
+int						duplicates(t_stack *stack);
+int						is_sorted(t_stack *stack);
 
-int			stack_len(t_stack *stack);
-int			duplicates(t_stack *stack);
-int			is_sorted(t_stack *stack);
+void					read_stdin(t_stack **stack_a, t_stack **stack_b);
 
-int			find_small_big(t_stack *stack, char *flag);
-void		interval(t_stack **stack_a, t_stack **stack_b, int min, int max);
-void		five_less(t_stack **stack_a, t_stack **stack_b);
-void		hundred_less(t_stack **stack_a, t_stack **stack_b);
-void		five_hundred_less(t_stack **stack_a, t_stack **stack_b);
+int						find_small_big(t_stack *stack, char *flag);
+void					range(t_stack **a, t_stack **b, int min, int max);
+void					five_less(t_stack **stack_a, t_stack **stack_b);
+void					hundred_less(t_stack **stack_a, t_stack **stack_b);
+void					five_hundred_less(t_stack **stack_a, t_stack **stack_b);
 
 #endif
