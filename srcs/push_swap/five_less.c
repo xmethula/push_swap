95d918910af9282d6f12e76c1fe002ca44f39d91
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-09-12 09:43:21 +0200

    srcs/

--- srcs/push_swap/five_less.c
+++ srcs/push_swap/five_less.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <xmethula@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/20 17:00:15 by xmethula          #+#    #+#             */
-/*   Updated: 2019/09/11 14:53:31 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/09/12 09:40:55 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,8 @@ static void		sort_four(t_stack **stack_a, t_stack **stack_b)
 		else
 			do_rra(*stack_a, 1);
 	}
+	if (is_sorted(*stack_a))
+		return ;
 	do_pb(stack_a, stack_b, 1);
 	sort_three(*stack_a);
 	do_pa(stack_a, stack_b, 1);
@@ -66,28 +68,31 @@ static void		sort_four(t_stack **stack_a, t_stack **stack_b)
 static void		sort_five(t_stack **stack_a, t_stack **stack_b)
 {
 	int			smallest;
-	t_stack		*i;
+	int			i;
+	t_stack		*p;
 
-	smallest = find_small_big(*stack_a, "sn");
-	i = (*stack_a);
-	while (i->num != smallest)
+	i = 0;
+	while (i < 2)
 	{
-		if (find_small_big(*stack_a, "snp") <= 2)
-			do_ra(*stack_a, 1);
-		else
-			do_rra(*stack_a, 1);
+		smallest = find_small_big(*stack_a, "sn");
+		p = (*stack_a);
+		while (p->num != smallest)
+		{
+			if (find_small_big(*stack_a, "snp") <= 2)
+				do_ra(*stack_a, 1);
+			else
+				do_rra(*stack_a, 1);
+		}
+		if ((is_sorted(*stack_a)) && (stack_len(*stack_b) == 0))
+			return ;
+		else if ((is_sorted(*stack_a)) && (stack_len(*stack_b) == 1))
+		{
+			do_pa(stack_a, stack_b, 1);
+			return ;
+		}
+		do_pb(stack_a, stack_b, 1);
+		i++;
 	}
-	do_pb(stack_a, stack_b, 1);
-	smallest = find_small_big(*stack_a, "sn");
-	i = (*stack_a);
-	while (i->num != smallest)
-	{
-		if (find_small_big(*stack_a, "snp") <= 2)
-			do_ra(*stack_a, 1);
-		else
-			do_rra(*stack_a, 1);
-	}
-	do_pb(stack_a, stack_b, 1);
 	sort_three(*stack_a);
 	do_pa(stack_a, stack_b, 1);
 	do_pa(stack_a, stack_b, 1);
